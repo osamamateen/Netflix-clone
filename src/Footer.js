0c318ddcede15760b2dd6bbbec098393ff5e389c
Osama <osamamateen23@gmail.com>
2020-07-20 18:33:02 -0700

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="footer__credits">Designed and Developed By Osama Mateen</p>
+    </div>
+  );
+};
+
+export default Footer;
